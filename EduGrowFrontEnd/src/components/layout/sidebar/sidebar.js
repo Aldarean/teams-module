0d810e1a5e92298aec75569ff85eb23c6c6e7d66
Rodney Wormsbecher <rodney22@live.nl>
2018-12-25 22:58:14 +0100

    updated the left nav bar to also have a mobile menu. Uses a resize watcher to close itself when the screen enlarges again.

--- EduGrowFrontEnd/src/components/layout/sidebar/sidebar.js
+++ EduGrowFrontEnd/src/components/layout/sidebar/sidebar.js
@@ -6,20 +6,32 @@ import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import People from '@material-ui/icons/People';
 import MeetingRoom from '@material-ui/icons/MeetingRoom';
 import DataUsage from '@material-ui/icons/DataUsage';
+import  Menu from '@material-ui/icons/Menu';
 
 class Sidebar extends Component {
 
-  render() {
+
+
+  state = {
+    showMobileMenu: false,
+    mobileNav: '',
+    showDesktopNav: true,
+    desktopNav: ''
+  };
+
+
+
+  componentDidMount() {
 
     let menuGeneral = [
-        {
-          type: 'menuItem',
-          icon: <Dashboard />,
-          text: 'Summary'
-        }
+      {
+        type: 'menuItem',
+        icon: <Dashboard />,
+        text: 'Summary'
+      }
     ];
 
-    let menuMenage = [
+    let menuManage = [
       {
         type: 'menuItem',
         icon: <LibraryBooks />,
@@ -37,32 +49,93 @@ class Sidebar extends Component {
       },
 
     ];
+      let menuScores = [
+        {
+          type: 'menuItem',
+          icon: <DataUsage />,
+          text: 'Scores'
+        }
+      ];
 
-    let menuScores = [
-      {
-        type: 'menuItem',
-        icon: <DataUsage />,
-        text: 'Scores'
-      }
-    ];
+
+    let desktopNav = (
+      <nav className="sidebar-container__nav">  
+        <CreateMenuService  menuData={menuGeneral} />
+
+        <h2>Manage</h2>
+        <CreateMenuService  menuData={menuManage} />
+        
+        <h2>Grades</h2>
+        <CreateMenuService  menuData={menuScores} />
+      </nav>
+    )
+
+    this.setState({ desktopNav: desktopNav});
+    this.setState({ mobileNav: ''});
+  }
+
+
+
+  render() {
+
+    let activateMobileMenu = () => {
+
+
+      if (this.state.showMobileMenu === false) {
+
+        const mobileNav = (
+          <nav className="sidebar-container__mobile-nav">
+            test
+          </nav>
+		);
+		
+        this.setState({ mobileNav: mobileNav});
+		this.setState({showDesktopNav: false})
+		
+      } else {
+      
+        this.setState({ mobileNav: ''});
+        this.setState({ showDesktopNav: true});
+        
+	  }
+	  
+	  this.setState({showMobileMenu: !this.state.showMobileMenu});
+	  console.log(this.state.showMobileMenu);
+	}
+	
+
+
+	window.addEventListener('resize', (event) => {
+		if (event.currentTarget.innerWidth > 575) {
+
+			if (this.state.showDesktopNav === false) {
+				console.log('i execute');
+				activateMobileMenu();
+				this.setState({ showDesktopNav: true});
+			}
+		
+		}
+	});
+
+
 
     return (
       <aside className="sidebar-container">
 
         <header className="sidebar-container__header">
           <h1>Edu <span>Grow</span></h1>
+
+          <div className="sidebar-container__mobile-menu" onClick={activateMobileMenu}>
+            <Menu />
+          </div>
+ 
         </header>
 
-        <nav className="sidebar-container__nav">  
-          <CreateMenuService  menuData={menuGeneral} />
-
-          <h2>Manage</h2>
-          <CreateMenuService  menuData={menuMenage} />
-          
-          <h2>Grades</h2>
-          <CreateMenuService  menuData={menuScores} />
-        </nav>
-
+      
+        {this.state.mobileNav}
+        {this.state.showDesktopNav ? this.state.desktopNav : null}
+      
+        
       </aside>
     )
   }
