51cafb6b15f938f3e034fbdcd59673ec97c7eac0
Rodney Wormsbecher <rodney22@live.nl>
2018-12-26 00:54:18 +0100

    finished the mobile menu. items are shown via an animation

--- EduGrowFrontEnd/src/components/layout/sidebar/sidebar.js
+++ EduGrowFrontEnd/src/components/layout/sidebar/sidebar.js
@@ -1,144 +1,148 @@
 import React, { Component } from 'react';
 import CreateMenuService from './../../../services/sidebar/createMenuService';
+import Slide from 'react-reveal/Slide';
 
 import Dashboard from '@material-ui/icons/Dashboard';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import People from '@material-ui/icons/People';
 import MeetingRoom from '@material-ui/icons/MeetingRoom';
 import DataUsage from '@material-ui/icons/DataUsage';
-import  Menu from '@material-ui/icons/Menu';
+import Menu from '@material-ui/icons/Menu';
 
 class Sidebar extends Component {
 
+	state = {
+		mobileNav: '',
+		showDesktopNav: true,
+		desktopNav: '',
+		menuGeneral: [
+			{
+				type: 'menuItem',
+				icon: <Dashboard />,
+				text: 'Summary'
+			}
+		],
+		menuManage: [
+			{
+				type: 'menuItem',
+				icon: <LibraryBooks />,
+				text: 'View classes'
+			},
+			{
+				type: 'menuItem',
+				icon: <People />,
+				text: 'Students'
+			},
+			{
+				type: 'menuItem',
+				icon: <MeetingRoom />,
+				text: 'Classrooms'
+			},
+		],
+		menuScores: [
+			{
+				type: 'menuItem',
+				icon: <DataUsage />,
+				text: 'Scores'
+			}
+		]
+	};
 
 
-  state = {
-    showMobileMenu: false,
-    mobileNav: '',
-    showDesktopNav: true,
-    desktopNav: ''
-  };
+	// is responsible for building the left menu initial state.
+  	componentDidMount() {
 
+		let desktopNav = (
+		<nav className="sidebar-container__nav">  
+			<CreateMenuService  menuData={this.state.menuGeneral} />
 
+			<h2>Manage</h2>
+			<CreateMenuService  menuData={this.state.menuManage} />
+			
+			<h2>Grades</h2>
+			<CreateMenuService  menuData={this.state.menuScores} />
+		</nav>
+		)
 
-  componentDidMount() {
-
-    let menuGeneral = [
-      {
-        type: 'menuItem',
-        icon: <Dashboard />,
-        text: 'Summary'
-      }
-    ];
-
-    let menuManage = [
-      {
-        type: 'menuItem',
-        icon: <LibraryBooks />,
-        text: 'View classes'
-      },
-      {
-        type: 'menuItem',
-        icon: <People />,
-        text: 'Students'
-      },
-      {
-        type: 'menuItem',
-        icon: <MeetingRoom />,
-        text: 'Classrooms'
-      },
-
-    ];
-      let menuScores = [
-        {
-          type: 'menuItem',
-          icon: <DataUsage />,
-          text: 'Scores'
-        }
-      ];
-
-
-    let desktopNav = (
-      <nav className="sidebar-container__nav">  
-        <CreateMenuService  menuData={menuGeneral} />
-
-        <h2>Manage</h2>
-        <CreateMenuService  menuData={menuManage} />
-        
-        <h2>Grades</h2>
-        <CreateMenuService  menuData={menuScores} />
-      </nav>
-    )
-
-    this.setState({ desktopNav: desktopNav});
-    this.setState({ mobileNav: ''});
-  }
+		this.setState({ desktopNav: desktopNav});
+		this.setState({ mobileNav: ''});
+  	}
 
 
 
-  render() {
+  	render() {
 
-    let activateMobileMenu = () => {
+		// toggles the mobile menu and updates the state to decide what to render;
+		let activateMobileMenu = () => {
+			if (this.state.showMobileMenu === false) 
+			{
+				const mobileNav = (
+					<div className="mobile-menu__wrapper">
+						<Slide left duration={600}>
+							<nav className="mobile-nav">
+								<CreateMenuService  menuData={this.state.menuGeneral} />
 
+								<h2>Manage</h2>
+								<CreateMenuService  menuData={this.state.menuManage} />
 
-      if (this.state.showMobileMenu === false) {
-
-        const mobileNav = (
-          <nav className="sidebar-container__mobile-nav">
-            test
-          </nav>
-		);
-		
-        this.setState({ mobileNav: mobileNav});
-		this.setState({showDesktopNav: false})
-		
-      } else {
-      
-        this.setState({ mobileNav: ''});
-        this.setState({ showDesktopNav: true});
-        
-	  }
-	  
-	  this.setState({showMobileMenu: !this.state.showMobileMenu});
-	  console.log(this.state.showMobileMenu);
-	}
-	
-
-
-	window.addEventListener('resize', (event) => {
-		if (event.currentTarget.innerWidth > 575) {
-
-			if (this.state.showDesktopNav === false) {
-				console.log('i execute');
-				activateMobileMenu();
+								<h2>Grades</h2>
+								<CreateMenuService  menuData={this.state.menuScores} />
+							</nav>
+						</Slide>
+					</div>
+				);
+			
+				this.setState({ mobileNav: mobileNav});
+				this.setState({showDesktopNav: false})
+			} 
+			else 
+			{
+				this.setState({ mobileNav: ''});
 				this.setState({ showDesktopNav: true});
 			}
-		
+			
+			this.setState({showMobileMenu: !this.state.showMobileMenu});
 		}
-	});
+		
+
+		// We use an event listener to close the mobile window if the window gets bigger than 575px and the menu is still open.
+		window.addEventListener('resize', (event) => {
+			if (event.currentTarget.innerWidth > 575) 
+			{
+				if (this.state.showDesktopNav === false)
+				{
+					console.log('i execute');
+					activateMobileMenu();
+					this.setState({ showDesktopNav: true});
+				}
+			}
+		});
 
 
 
-    return (
-      <aside className="sidebar-container">
+		return (
+			<div>
+				<aside className="sidebar-container">
 
-        <header className="sidebar-container__header">
-          <h1>Edu <span>Grow</span></h1>
+					<header className="sidebar-container__header">
+						<h1>Edu <span>Grow</span></h1>
 
-          <div className="sidebar-container__mobile-menu" onClick={activateMobileMenu}>
-            <Menu />
-          </div>
- 
-        </header>
+						<div className="sidebar-container__mobile-menu" onClick={activateMobileMenu}>
+							<Menu />
+						</div>
 
-      
-        {this.state.mobileNav}
-        {this.state.showDesktopNav ? this.state.desktopNav : null}
-      
-        
-      </aside>
-    )
-  }
+					</header>
+
+					{this.state.showDesktopNav ? this.state.desktopNav : null}
+
+				</aside>
+
+				{/* mobile nav */}
+				{this.state.mobileNav}
+			</div>
+			
+		)
+	}
 }
 
 export default Sidebar;
